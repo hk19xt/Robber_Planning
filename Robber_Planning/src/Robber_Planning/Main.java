07a3d3b0ca6450c133ec7e4830d22b1ffd837d4f
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-10-07 13:00:11 -0400

    Update Main.java

--- Robber_Planning/src/Robber_Planning/Main.java
+++ Robber_Planning/src/Robber_Planning/Main.java
@@ -2,6 +2,10 @@ package Robber_Planning;
 
 public class Main {
     public static void main(String[] args){
-        
+        Robber_Planning RP = new Robber_Planning();
+        int[] user_houses = {2,7,9,3,1};
+        int maximum_amount = 0;
+        maximum_amount = RP.planning(user_houses);
+        System.out.println(maximum_amount);
     }
 }
